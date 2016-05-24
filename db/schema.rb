0d62757b21e0e034ac1a12d50f444c44f1b6bfc7
admin <souffleur.nicolas@gmail.com>
2016-05-24 23:37:20 +0200

    Add product image import, add rent controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503151000) do
+ActiveRecord::Schema.define(version: 20160524211617) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -54,13 +54,17 @@ ActiveRecord::Schema.define(version: 20160503151000) do
   end
 
   create_table "produits", force: :cascade do |t|
-    t.string   "nom",           limit: 255
+    t.string   "nom",                        limit: 255
     t.boolean  "disponibilite"
-    t.text     "description",   limit: 65535
-    t.text     "image",         limit: 65535
-    t.string   "ean",           limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.text     "description",                limit: 65535
+    t.text     "image",                      limit: 65535
+    t.string   "ean",                        limit: 255
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.string   "product_image_file_name",    limit: 255
+    t.string   "product_image_content_type", limit: 255
+    t.integer  "product_image_file_size",    limit: 4
+    t.datetime "product_image_updated_at"
   end
 
   create_table "roles", force: :cascade do |t|
