2bebbb0c53126e02a9048d602d77b5fdde1b2d83
ThomasG17 <t.gauret17@gmail.com>
2016-05-25 09:57:37 +0200

    distinct User & admin User

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,4 @@
-class AdminUser < User
-  belongs_to :ability
-  has_many :locations
+class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503151000) do
+ActiveRecord::Schema.define(version: 20160525075235) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
