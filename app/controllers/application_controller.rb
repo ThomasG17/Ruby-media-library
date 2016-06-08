648f64ad5af5f56dc6db93786151a256d73bd2eb
Mathias-Rui Lopes <necrosis159@gmail.com>
2016-06-08 11:56:13 +0200

    Change language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_filter :set_locale
+	def set_locale
+	    I18n.locale = params[:locale] || I18n.default_locale
+	end
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
